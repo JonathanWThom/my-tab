7d1e7a3c67bd89936031072c20ae0007ab9d56db
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-08-12 21:47:54 -0700

    Adds create and get all store methods for drinks

---
+++ drink.go
@@ -0,0 +1,8 @@
+package main
+
+type Drink struct {
+	id       int
+	percent  float64
+	oz       float64
+	stddrink float64
+}
---
+++ store.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/jonathanwthom/my-tab/stddrink"
+)
+
+type Store interface {
+	CreateDrink(drink *Drink) error
+	GetDrinks() ([]Drink, error)
+}
+
+type dbStore struct {
+	db *sql.DB
+}
+
+func (store *dbStore) CreateDrink(drink *Drink) error {
+	drink.stddrink = stddrink.Calculate(drink.percent, drink.oz)
+	_, err := store.db.Exec("INSERT INTO drinks(percent, oz, stddrink) VALUES($1, $2, $3)",
+		drink.percent, drink.oz, drink.stddrink)
+	return err
+}
+
+func (store *dbStore) GetDrinks() ([]*Drink, error) {
+	rows, err := store.db.Query("SELECT id, percent, oz, stddrink FROM drinks")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	drinks := []*Drink{}
+	for rows.Next() {
+		drink := &Drink{}
+		if err := rows.Scan(&drink.id, &drink.percent, &drink.oz, &drink.stddrink); err != nil {
+			return nil, err
+		}
+
+		drinks = append(drinks, drink)
+	}
+
+	return drinks, nil
+}
