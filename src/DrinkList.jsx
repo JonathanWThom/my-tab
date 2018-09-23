c41fa2177aa2ee51fd8e61af6b25163dd63b45d1
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-09-23 08:54:56 -0700

    Adds rounding to amounts/averages, and prettifies dates

--- src/DrinkList.jsx
+++ src/DrinkList.jsx
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import moment from "moment";
 
 export default class DrinkList extends Component {
+  dateRange() {
+    const drinks = this.props.drinks;
+    if (drinks.length > 1) {
+      return `${this.formattedImbibedOn(drinks[0])} - ${this.formattedImbibedOn(drinks[drinks.length - 1])}`
+    } else if (drinks.length === 1) {
+      return this.formattedImbibedOn(drinks[0]);
+    }
+  }
+
   drinkCopy(drink) {
     return drink.stddrink === 1 ? "drink" : "drinks";
   }
@@ -10,13 +19,8 @@ export default class DrinkList extends Component {
     return moment(drink.imbibedOn).format("MMMM Do, YYYY");
   }
 
-  dateRange() {
-    const drinks = this.props.drinks;
-    if (drinks.length > 1) {
-      return `${drinks[0].imbibedOn} - ${drinks[drinks.length - 1].imbibedOn}`
-    } else if (drinks.length === 1) {
-      return drinks[0].imbibedOn
-    }
+  roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
   }
 
   render() {
@@ -25,17 +29,17 @@ export default class DrinkList extends Component {
         <p>
           {this.formattedImbibedOn(drink)}: &nbsp;
           {drink.oz} oz at {drink.percent * 100}% is &nbsp;
-          {drink.stddrink} standard &nbsp;
+          {this.roundToTwo(drink.stddrink)} standard &nbsp;
           { this.drinkCopy(drink) }
         </p>
       </div>
     )
     const dateRangeCopy = <p><strong>Date Range: </strong>{this.dateRange()}</p>
-    const totalCopy = <p><strong>Total Standard Drinks: </strong>{this.props.total}</p>;
+    const totalCopy = <p><strong>Total Standard Drinks: </strong>{this.roundToTwo(this.props.total)}</p>;
     const perDayCopy =
       <p>
         <strong>Average Standard Drinks Per Day: </strong>
-        {this.props.perDay}
+        {this.roundToTwo(this.props.perDay)}
       </p>;
 
 
