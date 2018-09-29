f72ea779df0e6db3a3a635997bdb388abff0647d
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-09-28 22:59:38 -0700

    Morrre linter goodness

--- src/DrinkForm.jsx
+++ src/DrinkForm.jsx
@@ -1,40 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export default class DrinkForm extends Component {
-  render() {
-    return(
-      <div>
-        <h4>Add Drink</h4>
-        <form id="drink-form" onSubmit={this.props.handleSubmit}>
-          <label name="oz">Ounces</label>
-          <input
-            value={this.props.oz}
-            onChange={this.props.handleInputChange}
-            name="oz"
-            type="number"
-            step="0.01"
-            />
-          <br />
-          <label name="percent">Percent</label>
-          <input
-            value={this.props.percent}
-            onChange={this.props.handleInputChange}
-            name="percent"
-            type="number"
-            step="0.01"
-            />
-          <br />
-          <label name="imbibedOn">Imbibed On</label>
-          <input
-            value={this.props.imbibedOn}
-            onChange={this.props.handleInputChange}
-            type="date"
-            name="imbibedOn"
-            />
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    )
-  }
-}
+const DrinkForm = (props) => {
+  const {
+    handleSubmit, oz, handleInputChange, percent, imbibedOn,
+  } = props;
+
+  return (
+    <div>
+      <h4>Add Drink</h4>
+      <form id="drink-form" onSubmit={handleSubmit}>
+        <label name="oz">Ounces</label>
+        <input
+          value={oz}
+          onChange={handleInputChange}
+          name="oz"
+          type="number"
+          step="0.01"
+        />
+        <br />
+        <label name="percent">Percent</label>
+        <input
+          value={percent}
+          onChange={handleInputChange}
+          name="percent"
+          type="number"
+          step="0.01"
+        />
+        <br />
+        <label name="imbibedOn">Imbibed On</label>
+        <input
+          value={imbibedOn}
+          onChange={handleInputChange}
+          type="date"
+          name="imbibedOn"
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+DrinkForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  oz: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  percent: PropTypes.string.isRequired,
+  imbibedOn: PropTypes.string.isRequired,
+};
+
+export default DrinkForm;
